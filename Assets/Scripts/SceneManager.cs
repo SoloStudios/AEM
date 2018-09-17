e232222a7067d0701e2baebf5d35b1f6e06c4658
Zachary Wade <d4rkenedmatter@gmail.com>
2018-09-17 19:38:08 -0400

    Added SceneManager and start of options

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public int playerX;
+    public int playerY;
+    public int currentFloor;
+    public List<TextAsset> floorFiles = new List<TextAsset>();
+    private List<string> playerOptions = new List<string>();
+    private string[][,] floorMaps = new string[10][,];
+
+    const string NoAction = "H0000";
+
+    //Rooms should be names as following: HWNES
+    //If there is no H then it is an actual room. 
+    //If it is a Hallway it should start with H and continue with a 1 or 0 for each directional movement possibility
+
+
+    // Use this for initialization
+    void Start()
+    {
+        for(int i = 0; i<floorFiles.Count - 1; i++) {
+            floorMaps[i] = LoadFloorFromFile(floorFiles[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CheckMoveOptions() {
+        string playerLocationID = floorMaps[currentFloor][playerX, playerY];
+        if (playerLocationID.Contains("H")) {
+            if(playerLocationID[1] == '1') {
+
+            }
+            if (playerLocationID[2] == '1') {
+
+            }
+            if (playerLocationID[3] == '1') {
+
+            }
+            if (playerLocationID[4] == '1') {
+
+            }
+        }
+    }
+
+    public string[,] LoadFloorFromFile(TextAsset textFile)
+    {
+        string[] rows = textFile.text.Split('\n');
+        string[,] floorMap = new string[rows.Length, rows[0].Length];
+        for (int x = 0; x < rows.Length; x++)
+        {
+            string[] row = rows[x].Split(' ');
+            for (int y = 0; y < row.Length; y++)
+            {
+                floorMap[x, y] = row[y];
+            }
+        }
+        return floorMap;
+    }
+}
+
--- Assets/Scripts/SpotType.cs
+++ Assets/Scripts/SpotType.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 
 public class SpotType {
-  const string NoAction = "000";
-  const string Left = "001";
-  const string Right = "002";
-  const string Straight = "003";
-  const string LeftRight = "004";
-  const string LeftStraight = "005";
-  const string LeftRightStraight = "006";
-  const string RightStraight = "007";
-  const string DeadEnd = "008";
+    const string NoAction = "000";
+    const string West = "001";
+    const string East = "002";
+    const string North = "003";
+    const string WestEast = "004";
+    const string WestNorth = "005";
+    const string WestNorthEast = "006";
+    const string NorthEast = "007";
+    const string DeadEnd = "008";
+    //Need to create values for all directions, for example westnortheastsouth
+    //Naming convention should follow starting at west going clockwise to keep consistency
+    //the top of the map is always north
 }
